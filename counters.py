8cde30c3c791e4da2167db7e3dc69c690999e8b7
doveyam <donyavlad7390@gmail.com>
2026-06-01 17:14:26 +0300

    Create counters.py

---
+++ counters.py
@@ -0,0 +1,55 @@
+COUNTERS = {
+    "Frank": [
+        "Janet",
+        "Amber",
+        "Penny",
+        "Colette",
+        "El Primo",
+        "Griff",
+        "Chester",
+        "Lumi",
+        "Rico"
+    ],
+
+    "Mortis": [
+        "Jacky",
+        "Shade",
+        "Bull",
+        "Shelly",
+        "Gale",
+        "Otis",
+        "Chester",
+        "Crow"
+    ],
+
+    "Leon": [
+        "Jacky",
+        "Bull",
+        "Shelly",
+        "Crow",
+        "Chester",
+        "Charlie",
+        "Emz",
+        "Mortis"
+    ],
+
+    "Crow": [
+        "Spike",
+        "Rosa",
+        "Gus",
+        "Byron",
+        "Piper",
+        "Charlie"
+    ],
+
+    "Chester": [
+        "Janet",
+        "Amber",
+        "Gale",
+        "Stu",
+        "Ruffs",
+        "Meeple",
+        "Emz",
+        "Charlie"
+    ]
+}
