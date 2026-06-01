e60d19d52aa46af428f4457619577249f8acc247
doveyam <donyavlad7390@gmail.com>
2026-06-01 23:37:40 +0300

    Create translations.py

---
+++ translations.py
@@ -0,0 +1,20 @@
+MAP_TRANSLATIONS = {
+    "Double Swoosh": "Вжух-вжух",
+    "Undermine": "Подрыв",
+
+    "Triple Dribble": "Трипл-дриблинг",
+    "Pinball Dreams": "Пинбол",
+
+    "Ring of Fire": "Огненное кольцо",
+    "Dueling Beetles": "Муравьиные бега",
+
+    "Out in the Open": "Открытая площадь",
+    "New Horizons": "Новые горизонты",
+    "Belle's Rock": "Скала Белль",
+
+    "Kaboom Canyon": "Пороховой каньон",
+
+    "Shooting Star": "Падающая звезда",
+    "Layer Cake": "Кремовый торт",
+    "Hideout": "Укрытие"
+}
