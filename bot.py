5d8a1b7084b1ea9060882212dbfe9151592baf59
doveyam <donyavlad7390@gmail.com>
2026-06-01 21:22:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elif data == "add_enemy":
 
-    keyboard = []
+        keyboard = []
 
     for brawler in sorted(COUNTERS.keys()):
 
