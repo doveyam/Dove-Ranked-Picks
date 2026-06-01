a682d3b6aa0703a4628816fbb2da9986832d6162
doveyam <donyavlad7390@gmail.com>
2026-06-01 14:59:23 +0300

    Update data.py

---
+++ data.py
@@ -0,0 +1,52 @@
+MODES = {
+    "gemgrab": {
+        "name": "💎 Gem Grab",
+        "maps": [
+            "Double Swoosh",
+            "Undermine",
+            "Кристальный форт"
+        ]
+    },
+    "brawlball": {
+        "name": "⚽ Brawl Ball",
+        "maps": [
+            "Triple Dribble",
+            "Pinball Dreams",
+            "Зловредные поля"
+        ]
+    },
+    "hotzone": {
+        "name": "🔥 Hot Zone",
+        "maps": [
+            "Ring of Fire",
+            "Dueling Beetles",
+            "Открыто!"
+        ]
+    },
+    "knockout": {
+        "name": "💀 Knockout",
+        "maps": [
+            "Out in the Open",
+            "New Horizons",
+            "Belle's Rock",
+            "Пылающий феникс",
+            "Бурные ключи"
+        ]
+    },
+    "heist": {
+        "name": "💰 Heist",
+        "maps": [
+            "Kaboom Canyon",
+            "Горячая кукуруза",
+            "Надёжное укрытие"
+        ]
+    },
+    "bounty": {
+        "name": "⭐ Bounty",
+        "maps": [
+            "Shooting Star",
+            "Layer Cake",
+            "Hideout"
+        ]
+    }
+}
